126ce8f758e70e2cdeb6a002dacbfae8edbd6d7a
hyeonwoongs <hyeonwoongs@gmail.com>
2024-02-13 09:37:54 +0900

    v01. combination

--- day20.py
+++ day20.py
@@ -1,32 +1,20 @@
-def print_poly(f_x) -> str:
-    term = len(f_x) - 1
-    poly_expression = "f(x) = "
-
-    for i in range(len(fx)):
-        coefficient = f_x[i]
-
-        if coefficient >= 0:
-            poly_expression = poly_expression + "+"
-        poly_expression = poly_expression + f'{coefficient}x^{term} '
-        term = term - 1
-
-    return poly_expression
+def factorial(number) -> int:
+    pass
 
 
-def calculation_poly(x_value, f_x) -> int:
-    return_value = 0
-    term = len(f_x) - 1
+def nCr(n, r) -> int:
+    '''
+    조합 함수
+    :param n:
+    :param r:
+    :return:
+    '''
+    numerator = factorial(n)
+    denominator = factorial(n-r) * factorial(r)
+    return numerator / denominator
 
-    for i in range(len(fx)):
-        coefficient = f_x[i]
-        return_value += coefficient * pow(x_value, term)
-        term = term - 1
-
-    return return_value
-
-
-fx = [2, 3, 4, 0, -9]
 
 if __name__ == "__main__":
-    print(print_poly(fx))
-    print(calculation_poly(int(input("x 값 : ")), fx))
+    n = int(input("Input n : "))
+    r = int(input("Input r : "))
+    print(f"{n}C{r} = {nCr(n, r)}")
